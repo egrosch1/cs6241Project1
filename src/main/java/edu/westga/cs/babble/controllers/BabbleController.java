e49208046d8c7a1b8fa931044d6d29f66f6f89c5
egrosch1 <egrosch1@csvlab-vm05.uwg.westga.edu>
2023-08-28 20:59:44 -0400

    Tiles working mostly.

--- src/main/java/edu/westga/cs/babble/controllers/BabbleController.java
+++ src/main/java/edu/westga/cs/babble/controllers/BabbleController.java
@@ -1,9 +1,10 @@
 package edu.westga.cs.babble.controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
-import javafx.util.Callback;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Button;
 import edu.westga.cs.babble.model.Tile;
 import edu.westga.cs.babble.model.TileBag;
 import edu.westga.cs.babble.model.EmptyTileBagException;
@@ -12,30 +13,17 @@ public class BabbleController {
 
     @FXML
     private ListView<Tile> tilesListView;
+  
 
     private TileBag tileBag;
+    private int currentIndex = 0;
 
     public BabbleController() {
         this.tileBag = new TileBag();
     }
 
     public void initialize() {
-        tilesListView.setCellFactory(new Callback<>() {
-            @Override
-            public ListCell<Tile> call(ListView<Tile> param) {
-                return new ListCell<>() {
-                    @Override
-                    protected void updateItem(Tile item, boolean empty) {
-                        super.updateItem(item, empty);
-                        if (item == null || empty) {
-                            setText(null);
-                        } else {
-                            setText(Character.toString(item.getLetter()));
-                        }
-                    }
-                };
-            }
-        });
+        tilesListView.setCellFactory(listView -> new TileListCell());
 
         try {
             for (int i = 0; i < 7; i++) {
@@ -44,6 +32,24 @@ public class BabbleController {
         } catch (EmptyTileBagException e) {
             System.err.println("The tile bag is empty.");
         }
+
     }
 
+    
+
+ 
+
+    private class TileListCell extends ListCell<Tile> {
+        @Override
+        protected void updateItem(Tile item, boolean empty) {
+            super.updateItem(item, empty);
+            if (item == null || empty) {
+                setText(null);
+                setGraphic(null);
+            } else {
+                Label tileLabel = new Label(Character.toString(item.getLetter()));
+                setGraphic(tileLabel);
+            }
+        }
+    }
 }
