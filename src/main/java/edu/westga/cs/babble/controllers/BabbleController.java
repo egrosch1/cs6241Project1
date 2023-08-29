412d5f77e1dad69dfd1f6cf4a17952357476ef62
egrosch1 <egrosch1@csvlab-vm07.uwg.westga.edu>
2023-08-29 16:47:41 -0400

    remove unused imports

--- src/main/java/edu/westga/cs/babble/controllers/BabbleController.java
+++ src/main/java/edu/westga/cs/babble/controllers/BabbleController.java
@@ -1,7 +1,5 @@
 package edu.westga.cs.babble.controllers;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
